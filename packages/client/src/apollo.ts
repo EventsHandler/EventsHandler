465605eb1688e7b19fb9d29a1e439c2a7d4a9ec3
cristichiu <grajdiancristian@gmail.com>
2025-05-03 22:19:01 +0300

    add context front/back

--- packages/client/src/apollo.ts
+++ packages/client/src/apollo.ts
@@ -1,10 +1,21 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 export const link = createHttpLink({
   uri: '/graphql',
 })
 
 export const apolloClient = new ApolloClient({
-  link,
+  link: authLink.concat(link),
   cache: new InMemoryCache(),
 })
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,16 @@ import { createServer } from 'http'
 import { typeDefs } from './schema/typeDefs.generated'
 import { resolvers } from './schema/resolvers.generated'
 
-const yoga = createYoga({ schema: createSchema({ typeDefs, resolvers }) })
+type MyContext = {
+  foo: string
+}
+
+const yoga = createYoga({
+  schema: createSchema<MyContext>({ typeDefs, resolvers }),
+  context: (): MyContext => {
+    // console.log("return some context")
+    return { foo: "context" }
+  }
+})
 const server = createServer(yoga)
 server.listen(process.env.PORT || 3000)
