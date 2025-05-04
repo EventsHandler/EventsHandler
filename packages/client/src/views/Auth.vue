50389a1ee9f3c71f7f63d3186c3e98684a48705e
Cornea Alexandrina <alexandrina.cornea05@gmail.com>
2025-05-04 14:01:40 +0300

    auth

--- packages/client/src/views/Auth.vue
+++ packages/client/src/views/Auth.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const username = ref('');
-const password = ref('');
+import Login from '@/components/Login.vue';
+import Register from '@/components/Register.vue';
+
+const switchLog = ref(true); 
+const switching = () => {
+  switchLog.value = !switchLog.value;
+};
 </script>
+
 <template>
-    <div class="login">
-        <div class="login-container">
-            <h1 class="login-title">Login</h1>
-            <div class="login-form">
-                <h1 class="username">Username:</h1>
-                <input type="text" v-model="username" class="login-input" placeholder="Enter your username" />
-                <h1 class="password">Password:</h1>
-                <input type="password" v-model="password" class="login-input" placeholder="Enter your password" />
-            </div>
-            <div class="login-button-container">
-                <button class="login-button">Login</button>
-        </div>
-      </div>
-    </div>
+  <Login v-if="switchLog" @switch="switching"/>
+
+  <Register v-else @switch="switching"/>
 </template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f7f7f7;
+}
+</style>
