3f7c401a78012c17ee631a801a01135ca0acf46f
cristichiu <grajdiancristian@gmail.com>
2025-05-04 11:30:14 +0300

    add some components and tempdb example

---
+++ packages/client/src/components/user/ProfileMini.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  name?: string
+}>()
+</script>
+
+<template>
+  <div>
+    <div>{{ name }}</div>   
+  </div>
+</template>
---
+++ packages/client/src/tempdb.ts
@@ -0,0 +1,31 @@
+export default [
+  {
+    title: "test1",
+    desc: "desc",
+    anounces: [],
+    participants: [
+      { username: "test" },
+      { username: "test2" }
+    ]
+  },
+  {
+    title: "test2",
+    desc: "desc2",
+    anounces: [
+      { title: "test", desc: "idk" }
+    ],
+    participants: [
+      { username: "test" },
+      { username: "test2" }
+    ]
+  },
+  {
+    title: "test3",
+    desc: "desc3",
+    anounces: [],
+    participants: [
+      { username: "test" },
+      { username: "test2" }
+    ]
+  },
+]
