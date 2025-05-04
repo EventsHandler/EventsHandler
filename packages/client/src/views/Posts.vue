109788be8e9c2b8c7f9c68ada2a617f9e988c9d5
dorina1233443323 <dorina.buruian05@gmail.com>
2025-05-04 10:46:20 +0300

    in work

---
+++ packages/client/src/views/Posts.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+            <h1 class="title">TITLE</h1>
+        </div>
+        <div>
+            <img class="image"/>
+        </div>
+        <div class="description">
+            <h2 id="description-name"></h2>
+            <p id="description-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. At doloremque explicabo accusantium delectus, porro veniam ipsam iste dicta id aperiam inventore in autem sequi veritatis et quae odit ipsa unde.</p>
+        </div>
+        <div>
+
+        </div>
+        <div></div>
+    </div>
+</template>
