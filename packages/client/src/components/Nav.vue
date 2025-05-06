54ebcb0daaa36dec273b1752be9167bff62a8c4e
DarkAshe <gavriloviustin2016@gmail.com>
2025-05-06 16:02:58 +0300

    NavBar Mobile

--- packages/client/src/components/Nav.vue
+++ packages/client/src/components/Nav.vue
@@ -1,24 +1,57 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import logo from '../assets/logo.svg'
+
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <template>
-<nav class="bg-blue-600 text-white w-full text-2xl">
-  <div class="max-w-7xl mx-auto flex items-center justify-between">
-    <RouterLink to="/" class="flex items-center">
-      <img src="/public/logo.svg" alt="logo" />
-    </RouterLink>
-    
-    <div class="flex items-center space-x-12">
-      <RouterLink to="/" class="hover:text-blue-200 transition">Home</RouterLink>
-      <RouterLink to="/about" class="hover:text-blue-200 transition">About</RouterLink>
-      <RouterLink to="/events" class="hover:text-blue-200 transition">Event list</RouterLink>
-    </div>
-  </div>
-</nav>
+  <nav class="bg-blue-600 text-white w-full text-2xl fixed">
+    <div class="max-w-7xl mx-auto px-4">
+      <div class="flex items-center justify-between">
+        <RouterLink to="/">
+          <img :src="logo" alt="logo" />
+        </RouterLink>
 
+        <button @click="toggleMenu" class="md:hidden inline-flex items-center justify-center">
+          <svg class="h-8 w-8" :class="{ 'hidden': isMenuOpen, 'block': !isMenuOpen }"
+            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+          <svg class="h-8 w-8" :class="{ 'block': isMenuOpen, 'hidden': !isMenuOpen }"
+            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+
+        <div class="hidden md:flex items-center space-x-12">
+          <RouterLink to="/about" class="hover:text-blue-200 transition">About</RouterLink>
+          <RouterLink to="/events" class="hover:text-blue-200 transition">Event list</RouterLink>
+          <RouterLink to="/auth" class="hover:text-blue-200 transition">Account</RouterLink>
+        </div>
+      </div>
+
+      <div class="md:hidden" :class="{ 'block': isMenuOpen, 'hidden': !isMenuOpen }">
+        <hr class="border-t-2">
+        <div class="pl-5 py-5 space-y-5">
+          <RouterLink to="/about" class="block hover:text-blue-200 transition" @click="isMenuOpen = false">
+            About
+          </RouterLink>
+          <RouterLink to="/events" class="block hover:text-blue-200 transition" @click="isMenuOpen = false">
+            Event list
+          </RouterLink>
+          <RouterLink to="/auth" class="block hover:text-blue-200 transition" @click="isMenuOpen = false">
+            Account
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<style scoped>
-nav { position: fixed }
-</style>
+<style scoped></style>
