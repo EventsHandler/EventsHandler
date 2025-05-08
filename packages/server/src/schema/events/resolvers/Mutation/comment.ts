e518061bf8a7c74a55a07719b6612395347d4128
cristichiu <grajdiancristian@gmail.com>
2025-05-08 22:52:24 +0300

    comments full setup added

--- packages/server/src/schema/events/resolvers/Mutation/comment.ts
+++ packages/server/src/schema/events/resolvers/Mutation/comment.ts
@@ -1,4 +1,13 @@
+import { prisma } from '../../../../prisma.js'
 import type { MutationResolvers } from './../../../types.generated.js'
-export const comment: NonNullable<MutationResolvers['comment']> = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.comment resolver logic here */
+export const comment: NonNullable<MutationResolvers['comment']> = async (_parent, { fromId, eventId, comment }, _ctx) => {
+  return await prisma.comment.create({
+    data: {
+      fromId, eventId, comment
+    },
+    include: {
+      from: true,
+      event: true
+    }
+  })
 }
