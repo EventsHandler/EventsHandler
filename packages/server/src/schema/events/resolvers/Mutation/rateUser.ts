ba214ca737d8a91e3214315d3229d45960b73484
cristichiu <grajdiancristian@gmail.com>
2025-05-08 16:10:48 +0300

    rate user finalizat

--- packages/server/src/schema/events/resolvers/Mutation/rateUser.ts
+++ packages/server/src/schema/events/resolvers/Mutation/rateUser.ts
@@ -1,4 +1,20 @@
+import { prisma } from '../../../../prisma.js'
 import type { MutationResolvers } from './../../../types.generated.js'
-export const rateUser: NonNullable<MutationResolvers['rateUser']> = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.rateUser resolver logic here */
+export const rateUser: NonNullable<MutationResolvers['rateUser']> = async (_parent, { fromId, toId, rate }, _ctx) => {
+  let exist = await prisma.rateing.findFirst({
+    where: { fromId, toId }
+  })
+  if(exist) {
+    await prisma.rateing.updateMany({
+      where: { fromId, toId },
+      data: { rate }
+    })
+  } else {
+    await prisma.rateing.create({
+      data: {
+        fromId, toId, rate
+      }
+    })
+  }
+  return null
 }
--- packages/server/src/schema/events/resolvers/User.ts
+++ packages/server/src/schema/events/resolvers/User.ts
@@ -1,4 +1,11 @@
+import { prisma } from '../../../prisma.js'
 import type { UserResolvers } from './../../types.generated.js'
 export const User: UserResolvers = {
-  /* Implement User resolver logic here */
+  myRates: async (_parent, _arg, _ctx) => {
+    return await prisma.rateing.findMany({
+      where: {
+        toId: _parent.id as string
+      }
+    })
+  }
 }
