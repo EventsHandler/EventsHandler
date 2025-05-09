fb391d822f9676b278b59cd9f762dfc8511271cd
cristichiu <grajdiancristian@gmail.com>
2025-05-09 16:44:43 +0300

    ai changes

--- packages/server/src/ai.ts
+++ packages/server/src/ai.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey: "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",
+  apiKey: process.env.API_AI_KEY,
 });
 export async function chatCompletition(input: string) {
   const completion: any = await openai.chat.completions.create({
