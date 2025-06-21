1acb5af437801d80c90aaf24fe9bcf4920d9d65e
dorina1233443323 <dorina.buruian05@gmail.com>
2025-06-21 20:35:40 +0300

    add permission based on group

--- packages/server/src/schema/events/resolvers/Mutation/chat_member_join.ts
+++ packages/server/src/schema/events/resolvers/Mutation/chat_member_join.ts
@@ -1,13 +1,31 @@
 import { prisma } from '../../../../prisma.js'
 import type { MutationResolvers } from './../../../types.generated.js'
 import { settings } from '../../../utils/settings/settings.js'
-export const chat_member_join: NonNullable<MutationResolvers['chat_member_join']> = async (_parent, { chatId }, _ctx) => {
-  return await prisma.chatMember.create({
-    data: {
-      userId: _ctx.user.id,
-      chatId,
-      accepted: await settings.has2({entityId: chatId, setting: "chat:autojoin", entityType: "chat"}),
-      // permissions: chat.defaultPerms @TODO
-    }
-  })
+import { memberPerms } from '../../../utils/permissions/member.js';
+export const chat_member_join: NonNullable<MutationResolvers['chat_member_join']> = async (_parent, { chatId, groupId }, _ctx) => {
+let permsToUse = memberPerms.default;
+   async function addWithPermissions(){ 
+    return await prisma.chatMember.create({
+      data: {
+        userId: _ctx.user.id,
+        chatId,
+        accepted: await settings.has2({entityId: chatId, setting: "chat:autojoin", entityType: "chat"}),
+        permissions: permsToUse
+      }
+    })
+  }
+  if(!groupId){
+    return await addWithPermissions();
+  }else{
+    const user = await prisma.groupMember.findUnique({
+      where: {
+        userId_groupId: {
+          userId: _ctx.user.id,
+          groupId: groupId
+        }
+      }
+    })
+    permsToUse = user?.permissions ?? memberPerms.default
+    return await addWithPermissions()
+  }
 }
--- packages/server/src/schema/events/resolvers/Mutation/event_member_join.ts
+++ packages/server/src/schema/events/resolvers/Mutation/event_member_join.ts
@@ -1,13 +1,30 @@
 import { prisma } from '../../../../prisma.js'
 import type { MutationResolvers } from './../../../types.generated.js'
 import { settings } from '../../../utils/settings/settings.js'
-export const event_member_join: NonNullable<MutationResolvers['event_member_join']> = async (_parent, { eventId }, _ctx) => {
-  return await prisma.eventMember.create({
-    data: {
-      userId: _ctx.user.id,
-      eventId,
+import { memberPerms } from '../../../utils/permissions/member.js';
+export const event_member_join: NonNullable<MutationResolvers['event_member_join']> = async (_parent, { eventId, groupId }, _ctx) => {
+  let permsToUse = memberPerms.default;
+   async function addWithPermissions(){ 
+    return await prisma.eventMember.create({
+      data: {
+      userId: _ctx.user.id, eventId,
       accepted: await settings.has2({entityId: eventId, setting: "event:autojoin", entityType: "event"}),
-      // permissions: @TODO
-    }
-  })
+        permissions: permsToUse
+      }
+    })
+  }
+  if(!groupId){
+    return await addWithPermissions();
+  }else{
+    const user = await prisma.groupMember.findUnique({
+      where: {
+        userId_groupId: {
+          userId: _ctx.user.id,
+          groupId: groupId
+        }
+      }
+    })
+    permsToUse = user?.permissions ?? memberPerms.default
+    return await addWithPermissions()
+  }
 }
